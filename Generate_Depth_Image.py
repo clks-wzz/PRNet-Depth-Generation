7832e93518b6b903364fdccd535d4517df01e71e
mehdi samadiniya <mehdisamadiniya@gmail.com>
2022-05-29 16:49:33 +0430

    update file

--- Generate_Depth_Image.py
+++ Generate_Depth_Image.py
@@ -1,30 +1,59 @@
-import numpy as np
-import scipy.io as sio
-from skimage.io import imread, imsave
 import cv2
-import os
+import time
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+from skimage.transform import resize
+from multiprocessing import Pool, cpu_count
 
 from api import PRN
+from config.pipeline import *
 import utils.depth_image as DepthImage
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-prn = PRN(is_dlib = True, is_opencv = False) 
+prn = PRN()
 
-path_image = './TestImages/0.jpg'
 
-image = imread(path_image)
-image_shape = [image.shape[0], image.shape[1]]
+def main():
+    datasets_list = glob(f'{datasets_dir}/*.npy')
 
-pos = prn.process(image, None, None, image_shape)
+    for dataset in tqdm(datasets_list):
+        ds = np.load(dataset, allow_pickle=True)
+        labels = ds[:10, 0]
+        datas = ds[:10, 1]
+        idx = list(range(len(labels)))
 
-kpt = prn.get_landmarks(pos)
 
-# 3D vertices
-vertices = prn.get_vertices(pos)
+        # MultiProcessing
+        pool = Pool(processes=cpu_count())
+        X = pool.starmap(build_map, zip(labels, datas, idx))
+        pool.close()
+        pool.join()
+        save_file_name = dataset.split("\\")[-1].split(".")[0]
+        np.save(f'{save_path}/{save_file_name}_W_depth.npy', np.array(X, dtype=object))
 
-depth_scene_map = DepthImage.generate_depth_image(vertices, kpt, image.shape, isMedFilter=True)
 
-cv2.imshow('IMAGE', image[:,:,::-1])
-cv2.imshow('DEPTH', depth_scene_map)
-cv2.waitKey(3000)
+def build_map(label, image, idx):
+    if label == 0:
+        depth = np.zeros((32, 32))
+    else:
+        pos = prn.net_forward(image / 255.)
+        # 3D vertices
+        vertices = prn.get_vertices(pos)
+        depth_scene_map = DepthImage.generate_depth_image(vertices, image.shape, is_med_filter=True)
+        depth = resize(depth_scene_map,
+                       output_shape=(32, 32),
+                       preserve_range=True)
+
+    cv2.imwrite(f'predict/{idx}.jpg', depth)
+
+    return [label, image, depth]
+
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    main()
+
+    end = time.perf_counter()
+    print('*' * 50)
+    print(round(end - start))
---
+++ config/pipeline.py
@@ -0,0 +1,4 @@
+dataset_name = 'roseyoutu'
+datasets_dir = r'C:\datasets\roseyoutu'
+save_path = r'C:\datasets'
+img_path = r'C:\images'
